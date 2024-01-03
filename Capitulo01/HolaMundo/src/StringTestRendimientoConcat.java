3b5b543ae9135abeee4eb4a5d2625ec65e26eb04
zedrikkPC <zedrikk_26_01@hotmail.com>
2024-01-03 01:13:15 -0500

    RendimientoString

---
+++ Capitulo01/HolaMundo/src/StringTestRendimientoConcat.java
@@ -0,0 +1,14 @@
+public class StringTestRendimientoConcat {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = a;
+
+        long inicio = System.currentTimeMillis();
+        long fin = System.currentTimeMillis();
+
+
+
+
+    }
+}
