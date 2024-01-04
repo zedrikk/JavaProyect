ab670bdc4836dec79bea573743593905577f9270
zedrikkPC <zedrikk_26_01@hotmail.com>
2024-01-04 01:42:39 -0500

    finalStringRendimiento

--- Capitulo01/HolaMundo/src/StringTestRendimientoConcat.java
+++ Capitulo01/HolaMundo/src/StringTestRendimientoConcat.java
@@ -4,8 +4,19 @@ public class StringTestRendimientoConcat {
         String b = "b";
         String c = a;
 
+        StringBuilder sb = new StringBuilder(a);
         long inicio = System.currentTimeMillis();
+        for (int i = 0; i < 500; i++){
+            //c = c.concat(a).concat(b).concat("\n"); => 2ms
+            //c += a + b + "\n"; => 19ms
+            sb.append(a).append(b).append("\n"); // 0ms
+
+        }
+
         long fin = System.currentTimeMillis();
+        System.out.println(fin - inicio);
+        System.out.println("c = " + c);
+        System.out.println("sb = " + sb.toString());
 
 
 
